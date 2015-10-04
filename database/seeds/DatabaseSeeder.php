285f3620ec355fb6b6fa1c0007a895bbdb0c96a3
Krasho <crasho1481@gmail.com>
2015-10-04 11:48:29 -0500

    I created Post Seeder and I changed models to Models Folder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder {
 		Model::unguard();
 
 		// $this->call(UserTableSeeder::class);
+		$this->call(PostTableSeeder::class);
 
 		$this->_seedToTag();
 		Model::reguard();
