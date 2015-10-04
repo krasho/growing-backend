630d1346f8b6ef92b1c48765f9ba56c09f0bb68c
Krasho <crasho1481@gmail.com>
2015-10-04 11:44:21 -0500

    I created seeder to Tag

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,21 +1,35 @@
 <?php
 
-use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        Model::unguard();
+class DatabaseSeeder extends Seeder {
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run() {
+		Model::unguard();
 
-        // $this->call(UserTableSeeder::class);
+		// $this->call(UserTableSeeder::class);
 
-        Model::reguard();
-    }
+		$this->_seedToTag();
+		Model::reguard();
+	}
+
+	private function _seedToTag() {
+		//Create Records
+		$tag = new Tag;
+		$tag->name = "N.L.P.";
+		$tag->save();
+
+		$tag = new Tag;
+		$tag->name = "Gestalt";
+		$tag->save();
+
+		$tag = new Tag;
+		$tag->name = "Humanist";
+		$tag->save();
+	}
 }
