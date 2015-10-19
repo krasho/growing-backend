34a02e4568721b3786dc640303a6956f2ebfb401
Krasho <crasho1481@gmail.com>
2015-10-19 14:47:58 -0500

    I fixed Invalid argument supplied for foreach() error

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,53 +8,56 @@
          </div>
        @endif
 
+       @if(count($phrase)>0)
        <div class="phrase">
            @foreach ($phrase as $p)
               <p>{{ $p->name }} <span>{{ $p->author }}</span></p>
            @endforeach
 
        </div>
+       @endif
    </section>
 
    <section class="posts">
-       @foreach ($posts as $post)
-         <div class="post">
-            <div class="header">
-                <div class="title">
-                    <a href="{{url('/post', $post->id) }}">{{ $post->title }}</a>
-                </div>
-                <div class="post-information">
-                    <div class="author">
-                        {{ $post->author }}
-                    </div>
-                    <div class="publish-date">
-                        {{ $post->publish_date }}
-                    </div>
-                    <div class="comments">
-                        {{ $post->commentsCount }} Comentarios
-                    </div>
-                </div>
-            </div>
-            <div class="body">
-                @if ($post->image)
-                  <div class="image-post">
-                      Imagen del post
+       @if(count($posts)>0)
+         @foreach ($posts as $post)
+           <div class="post">
+              <div class="header">
+                  <div class="title">
+                      <a href="{{url('/post', $post->id) }}">{{ $post->title }}</a>
                   </div>
-                @endif
+                  <div class="post-information">
+                      <div class="author">
+                          {{ $post->author }}
+                      </div>
+                      <div class="publish-date">
+                          {{ $post->publish_date }}
+                      </div>
+                      <div class="comments">
+                          {{ $post->commentsCount }} Comentarios
+                      </div>
+                  </div>
+              </div>
+              <div class="body">
+                  @if ($post->image)
+                    <div class="image-post">
+                        Imagen del post
+                    </div>
+                  @endif
 
 
-                <div class="content">
-                  {{ $post->short_description }}
-                </div>
+                  <div class="content">
+                    {{ $post->short_description }}
+                  </div>
 
-                <div class="link-post">
-                  <a href="{{url('/post', $post->id) }}">Ver Post</a>
-                </div>
-            </div>
+                  <div class="link-post">
+                    <a href="{{url('/post', $post->id) }}">Ver Post</a>
+                  </div>
+              </div>
 
-         </div>
+           </div>
 
-       @endforeach
-
+         @endforeach
+       @endif
    </section>
 @stop
