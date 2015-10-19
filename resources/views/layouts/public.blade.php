585c47eb4e14ba23ac01a4ed002794880de7c784
Krasho <crasho1481@gmail.com>
2015-10-19 14:55:23 -0500

    I added  faker in production enviroment

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -47,8 +47,6 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 
     {!! HTML::script('js/menu.js') !!}
-    {!! HTML::script('js/ckeditor/ckeditor.js') !!}
-
 
 </body>
 </html>
