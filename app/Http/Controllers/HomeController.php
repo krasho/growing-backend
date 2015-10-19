9b36ff3e8491fae5979fb98297e79ec1bae8c3ec
Krasho <crasho1481@gmail.com>
2015-10-19 14:23:38 -0500

    I set database configuration 13

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,9 +14,6 @@ class HomeController extends Controller {
 	 * @return \Illuminate\Http\Response
 	 */
 	public function index() {
-
-		var_dump(parse_url(getenv("DATABASE_URL")));
-
 		$event = Event::getEventOnDate();
 		$phrase = Phrase::getRandomPhrase();
 
