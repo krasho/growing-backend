5604b65dd6f0c39225544bf48be76d52536900c4
Krasho <crasho1481@gmail.com>
2015-10-19 14:42:26 -0500

    I fixed Trying to get property of non-object error in HomeController.php line 27: 02

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -29,7 +29,7 @@ class HomeController extends Controller {
 			"event" => $event,
 			"phrase" => $phrase,
 			"posts" => $posts,
-			"pathFile" => $this->_getFile($event->image),
+			"pathFile" => $pathFile,
 		));
 
 	}
