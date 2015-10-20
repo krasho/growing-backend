70c11b0babc7706dc26c9d655de5f8fc89aa6fd0
Krasho <crasho1481@gmail.com>
2015-10-20 08:33:16 -0500

    I added secure call to js

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -47,6 +47,7 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 
     {!! HTML::script('js/menu.js') !!}
+    {!! Html::script('js/menu.js',array(), true) !!}
 
 </body>
 </html>
